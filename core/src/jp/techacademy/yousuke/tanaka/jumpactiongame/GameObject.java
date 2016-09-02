9d58d8036b67a8b2b8c3fa4a9d604058cf839c73
yosuke-tanaka <y_tanaka_ym@yahoo.co.jp>
2016-09-03 00:02:33 +0900

    「6.7 オブジェクトを配置する」まで

---
+++ core/src/jp/techacademy/yousuke/tanaka/jumpactiongame/GameObject.java
@@ -0,0 +1,18 @@
+package jp.techacademy.yousuke.tanaka.jumpactiongame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * GameObjectクラスはSpriteクラスを継承し、Vector2クラスをメンバ変数に持つクラスとします。
+ */
+public class GameObject extends Sprite {
+    public final Vector2 velocity;  // x方向、y方向の速度を保持する
+
+    public GameObject(Texture texture, int srcX, int srcY, int srcWidth, int srcHeight) {
+        super(texture, srcX, srcY, srcWidth, srcHeight);
+
+        velocity = new Vector2();
+    }
+}
---
+++ core/src/jp/techacademy/yousuke/tanaka/jumpactiongame/GameScreen.java
@@ -0,0 +1,231 @@
+package jp.techacademy.yousuke.tanaka.jumpactiongame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameScreen extends ScreenAdapter {
+    // カメラのサイズを表す定数
+    static final float CAMERA_WIDTH = 10;
+    static final float CAMERA_HEIGHT = 15;
+
+    static final float WORLD_WIDTH = 10;
+    static final float WORLD_HEIGHT = 15 * 20; // 20画面分登れば終了
+
+    static final int GAME_STATE_READY = 0;
+    static final int GAME_STATE_PLAYING = 1;
+    static final int GAME_STATE_GAMEOVER = 2;
+
+    // 重力
+    static final float GRAVITY = -12;
+
+    private JumpActionGame mGame;
+
+    Sprite mBg;
+
+    // カメラを表すOrthographicCameraクラスと、ビューポートのFitViewportクラスをメンバ変数として定義
+    OrthographicCamera mCamera;
+    FitViewport mViewPort;
+
+    Random mRandom;
+    List<Step> mSteps;
+    List<Star> mStars;
+    Ufo mUfo;
+    Player mPlayer;
+
+    int mGameState;
+
+    /**
+     * 各画面に相当するScreenはScreenAdapterクラスを継承します。
+     * コンストラクタでは引数で受け取ったJumpActionGameクラスのオブジェクトをメンバ変数に保持します。
+     *
+     * その他重要なクラスはSpriteクラスとTextureクラスです。
+     * Spriteクラスは名前の通りスプライトです。
+     * スプライトとはコンピュータの処理の負荷を上げずに高速に画像を描画する仕組みです。
+     * 「プレイヤーや地面などの画像を表示するためのもの」という認識で問題ありません。
+     * Textureクラスはテクスチャを表すクラスで、スプライトに貼り付ける画像のことです。
+     * Textureクラスは画像のファイル名を指定して生成します。
+     * そして、SpriteクラスにTextureRegionクラスのオブジェクトとして生成したものを指定します。
+     * TextureRegionクラスはテクスチャとして用意した画像の一部を切り取ってスプライトに貼り付けるためのものです。
+     * なぜ、切り取る必要があるのでしょうか？
+     * 指定したback.pngが実際にどのような画像かを実際にAndroid Studio上で開いて確認してみましょう。
+     *
+     * Textureクラスで指定する画像は縦横は2の累乗、すなわち1,2,4,8,16,32,64,128,256,512,1024のような数字である必要があるのです。
+     * そのため、背景画像は 540 x 810 で作成したとしても画像ファイルとしては 1024 x 1024 である必要があり、
+     * このように切り出すことになります。
+     * 今回は1ファイルにつき1つの画像としていますが、例えばback.pngは右半分には多くのスペースがあります。
+     * ここに他の画像、例えばプレイヤーの画像などを用意しておき、1つの画像ファイルを読み込んだTextureクラスのオブジェクトから、
+     * 複数回別の位置の画像を切り出して別々のスプライトに指定するという使い方もあります。
+     *
+     * SpriteクラスのsetPositionメソッドで描画する位置を指定します。左下を基準として位置を指定します。
+     *
+     * -----
+     *
+     * @param game
+     */
+    public GameScreen(JumpActionGame game) {
+        mGame = game;
+
+        // 背景の準備
+        Texture bgTexture = new Texture("back.png");
+        // TextureRegionで切り出す時の原点は左上
+        mBg = new Sprite( new TextureRegion(bgTexture, 0, 0, 540, 810));
+        mBg.setSize(CAMERA_WIDTH, CAMERA_HEIGHT);
+        mBg.setPosition(0, 0);
+
+        // カメラ、ViewPortを生成、設定する
+        // コンストラクタでこれらメンバ変数に初期化して代入します。
+        // ポイントはカメラのサイズとビューポートのサイズをどちらもCAMERA_WIDTHとCAMERA_HEIGHTを使って同じにするということです。
+        // どちらも同じにしているため縦横比が固定されます。縦横比が固定されるので物理ディスプレイの比率と異なる場合は上下または左右に隙間ができるということです。
+        // 実行する際に複数の端末で試すか、様々な解像度のエミュレータを作成して試すとよく分かることでしょう。
+        mCamera = new OrthographicCamera();
+        mCamera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+        mViewPort = new FitViewport(CAMERA_WIDTH, CAMERA_HEIGHT, mCamera);
+
+        // メンバ変数の初期化
+        mRandom = new Random();
+        mSteps = new ArrayList<Step>();
+        mStars = new ArrayList<Star>();
+        mGameState = GAME_STATE_READY;
+
+        createStage();
+    }
+
+    /**
+     *コンストラクタで準備したスプライトをrenderメソッド内で描画します。
+     * ScreenAdapterを継承したクラスのrenderメソッドは基本的に1/60秒ごとに自動的に呼び出されます。
+     * 性能の低い古いAndroid端末や処理が重たい場合にはもう少し間隔が伸びる場合もあります。
+     *
+     * Gdx.gl.glClearColor(0, 0, 0, 1);と Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);で画面を描画する準備を行います。
+     * glClearColorメソッドは画面をクリアする時の色を赤、緑、青、透過で指定します。
+     * そしてGdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);で実際にその色でクリア（塗りつぶし）を行います。
+     *
+     * もう１つ大事なルールはスプライトなどを描画する際はSpriteBatchクラスのbeginメソッドとendメソッドの間で行うというルールです。
+     * ここではJumpActionGameクラスのメンバ変数で保持しているSpriteBatchクラスのオブジェクトを使います。
+     * そしてSpriteクラスのdrawメソッドを呼び出すことで描画します。
+     *
+     * --------
+     *
+     *カメラのupdateメソッドを呼び出し、
+     * SpriteBatchクラスのsetProjectionMatrixメソッドをOrthographicCameraクラスのcombinedプロパティを引数に与えて呼び出します。
+     * これはカメラの座標をアップデート（計算）し、スプライトの表示に反映させるために必要な呼び出しです。
+     * これらの呼び出しによって物理ディスプレイに依存しない表示を行うことができます。
+     * カメラのupdateメソッドでは行列演算を行ってカメラの座標値の再計算を行ってくれるメソッドです。
+     * そしてsetProjectionMatrixメソッドとcombinedメソッドでその座標をスプライトに反映しています。
+     *
+     *
+     * @param delta
+     */
+    @Override
+    public void render (float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // それぞれの状態をアップデートする
+        update(delta);
+
+        // カメラの座標をアップデート（計算）し、スプライトの表示に反映させる
+        mCamera.update();
+        mGame.batch.setProjectionMatrix(mCamera.combined);
+
+        mGame.batch.begin();
+
+        // 背景
+        // 原点は左下
+        mBg.setPosition(mCamera.position.x - CAMERA_WIDTH / 2, mCamera.position.y - CAMERA_HEIGHT / 2);
+        mBg.draw(mGame.batch);
+
+        // Step
+        for (int i = 0; i < mSteps.size(); i++) {
+            mSteps.get(i).draw(mGame.batch);
+        }
+
+        // Star
+        for (int i = 0; i < mStars.size(); i++) {
+            mStars.get(i).draw(mGame.batch);
+        }
+
+        // UFO
+        mUfo.draw(mGame.batch);
+
+        //Player
+        mPlayer.draw(mGame.batch);
+
+        mGame.batch.end();
+    }
+
+    /**
+     * 最後にresizeメソッドをオーバーライドしてFitViewportクラスのupdateメソッドを呼び出します。resizeメソッドは物理的な画面のサイズが変更されたときに呼び出されるメソッドです。Androidではcreate直後やバックグランドから復帰したときに呼び出されます。
+     *
+     * @param width
+     * @param height
+     */
+    @Override
+    public void resize(int width, int height) {
+        mViewPort.update(width, height);
+    }
+
+
+    // ステージを作成する
+    private void createStage() {
+
+        // テクスチャの準備
+        Texture stepTexture = new Texture("step.png");
+        Texture starTexture = new Texture("star.png");
+        Texture playerTexture = new Texture("uma.png");
+        Texture ufoTexture = new Texture("ufo.png");
+
+        // StepとStarをゴールの高さまで配置していく
+        float y = 0;
+
+        float maxJumpHeight = Player.PLAYER_JUMP_VELOCITY * Player.PLAYER_JUMP_VELOCITY / (2 * -GRAVITY);
+        while (y < WORLD_HEIGHT - 5) {
+            int type = mRandom.nextFloat() > 0.8f ? Step.STEP_TYPE_MOVING : Step.STEP_TYPE_STATIC;
+            float x = mRandom.nextFloat() * (WORLD_WIDTH - Step.STEP_WIDTH);
+
+            Step step = new Step(type, stepTexture, 0, 0, 144, 36);
+            step.setPosition(x, y);
+            mSteps.add(step);
+
+            if (mRandom.nextFloat() > 0.6f) {
+                Star star = new Star(starTexture, 0, 0, 72, 72);
+                star.setPosition(step.getX() + mRandom.nextFloat(), step.getY() + Star.STAR_HEIGHT + mRandom.nextFloat() * 3);
+                mStars.add(star);
+            }
+
+            y += (maxJumpHeight - 0.5f);
+            y -= mRandom.nextFloat() * (maxJumpHeight / 3);
+        }
+
+        // Playerを配置
+        mPlayer = new Player(playerTexture, 0, 0, 72, 72);
+        mPlayer.setPosition(WORLD_WIDTH / 2 - mPlayer.getWidth() / 2, Step.STEP_HEIGHT);
+
+        // ゴールのUFOを配置
+        mUfo = new Ufo(ufoTexture, 0, 0, 120, 74);
+        mUfo.setPosition(WORLD_WIDTH / 2 - Ufo.UFO_WIDTH / 2, y);
+    }
+
+    // それぞれのオブジェクトの状態をアップデートする
+    private void update(float delta) {
+        switch (mGameState) {
+            case GAME_STATE_READY:
+                break;
+            case GAME_STATE_PLAYING:
+                break;
+            case GAME_STATE_GAMEOVER:
+                break;
+        }
+    }
+
+
+}
